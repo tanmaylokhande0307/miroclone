8da02870878e3b4edfceae17a332b4d737068be5
debian <tl@zasmlabs.com>
2024-07-12 03:08:18 +0530

    created first convex mutation

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,31 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useOrganization } from "@clerk/nextjs";
+import { useApiMutations } from "@/hooks/use-api-mutations";
+import { toast } from "sonner";
 
 export const EmptyBoards = () => {
+  const { mutate, pending } = useApiMutations(api.board.create);
+  const { organization } = useOrganization();
+
+  const onClick = () => {
+    if (!organization) return;
+
+    mutate({
+      orgId: organization?.id,
+      title: "New Board",
+    })
+      .then((id) => {
+        toast.success("Board created");
+      })
+      .catch(() => {
+        toast.error("Failed to create board");
+      });
+  };
+
   return (
     <div className="flex flex-col h-full items-center justify-center">
       <Image
@@ -15,7 +39,9 @@ export const EmptyBoards = () => {
         Start by creating a board for your organization
       </p>
       <div className="mt-6">
-        <Button>Create Board</Button>
+        <Button onClick={onClick} disabled={pending}>
+          Create Board
+        </Button>
       </div>
     </div>
   );
---
+++ hooks/use-api-mutations.ts
@@ -0,0 +1,20 @@
+import { useMutation } from "convex/react";
+import { useState } from "react";
+
+export const useApiMutations = (mutationFunction: any) => {
+  const [pending, setPending] = useState(false);
+
+  const apiMutation = useMutation(mutationFunction);
+
+  const mutate = (payload: any) => {
+    setPending(true);
+    return apiMutation(payload)
+      .finally(() => setPending(false))
+      .then((result) => result)
+      .catch((err) => {
+        throw err;
+      });
+  };
+
+  return { mutate, pending };
+};
