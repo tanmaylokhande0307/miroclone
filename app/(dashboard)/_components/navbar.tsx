24ffe70279b59df2c2bd4c5f1b32eb0ba74b7e46
debian <tl@zasmlabs.com>
2024-07-12 01:18:52 +0530

    added debounced value to enter search query

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { SearchInput } from "./search-input";
 
 export const Navbar = () => {
   return (
-    <div className="flex item-center gap-x-4 p-5 bg-green-500">
-      <div className="hidden lg:flex lg:flex-1"></div>
+    <div className="flex item-center gap-x-4 p-5">
+      <div className="hidden lg:flex lg:flex-1">
+        <SearchInput />
+      </div>
 
       <UserButton />
     </div>
