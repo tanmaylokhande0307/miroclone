54cddc2c57591aa191225f47d31d3767bf936a9d
debian <tl@zasmlabs.com>
2024-07-14 01:46:55 +0530

    added basic layout of the canvas

---
+++ app/board/[boardId]/_components/participants.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Participants = () => {
+  return (
+    <div className='absolute h-12 top-2 right-2 p-3 rounded-md flex bg-white items-center shadow-md'>participants</div>
+  )
+}
